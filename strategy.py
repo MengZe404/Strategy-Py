cfbed980e314576303816dfd61feb9dd382c6134
MengZe <59787257+sudo-MEnGzE@users.noreply.github.com>
2020-06-07 15:44:05 +0800

    Version 2 - Better gameplay
    Thanks to jklol111's issue!

--- strategy.py
+++ strategy.py
@@ -91,13 +91,17 @@ class commands(cmd.Cmd):
     def do_status(self, player):
         '''
         status(arg): check the status (resources & populations & statistics) of a specific kingdom
-        Available arguments: me, Nusta, Lozar'''
+        Available arguments: me, nusta, lozar, all'''
         if player.lower() == '(me)':
-            print(myStatus)
+            print(my_status)
         elif player.lower() == '(nusta)':
-            print(nustaStatus)
+            print(nusta_status)
         elif player.lower() == '(lozar)':
-            print(lozarStatus)
+            print(lozar_status)
+        elif player.lower() == '(all)':
+            print("My: " + str(my_status))
+            print("Lozar: " + str(lozar_status))
+            print("Nusta: " + str(nusta_status))
         else:
             print('\n<System> Error: Invalid argument')
 
@@ -133,10 +137,11 @@ class commands(cmd.Cmd):
         game().action_Move()
 
 
+
 # The actual game
 class game:
     def __init__(self):
-        self.inventory_list = ['(piece of gold)', '(small package)']
+        self.inventory_list = ['(bag of gold)', '(mysterious package)', '(potion)']
 
 ##### The amount of resources issued to the player and opponents, calculated based on the difficulty of the game
     def initialise(self):
@@ -155,61 +160,106 @@ class game:
 
 ######## Default values
         global my_inventory
-        global myStatus
-        global nustaStatus
-        global lozarStatus
+        global my_status
+        global nusta_status
+        global lozar_status
 
         my_inventory = {
-            '(piece of gold)': 0,
-            '(small package)': 0
+            '(bag of gold)': 0,
+            '(mysterious package)': 0,
+            '(potion)': 0
         }
 
 ######## Default Resources of the player are inversly proportional to the difficulty
-        myStatus = {
+        my_status = {
             'gold': int(1000/self.difficulty_index),
             'soldier': int(5000/self.difficulty_index),
+            'HP': 100,
+            'defend': 1,
             'turn': 0,
             'victory': 0
         }
 
 ######### Default Resources of the opponents are directly proportional to the difficulty
-        nustaStatus = {
+        nusta_status = {
             'gold': int(1000*self.difficulty_index),
-            'soldier': int(5000*self.difficulty_index)
+            'soldier': int(5000*self.difficulty_index),
+            'HP': 100,
+            'defend': 1
         }
 
-        lozarStatus = {
+        lozar_status = {
             'gold': int(1000*self.difficulty_index),
-            'soldier': int(4000*self.difficulty_index)
+            'soldier': int(4000*self.difficulty_index),
+            'HP': 100,
+            'defend': 1
         }
 
 #### Events
     def event(self):
         global event
         # Do not trigger the encounter_Oppo event before 30 moves
-        if myStatus['turn'] <= 30:
-            event_index = str(random.randint(1, len(event[0])))
+        if my_status['turn'] <= 50:
+            posibility = random.randint(1,10)
+            if posibility >= 9:
+                event_index = str(random.randint(1, len(event[0])-2))
+            else:
+                event_index = str(random.randint(1, len(event[0])-5))
         else:
             event_index = str(random.randint(1, len(event[0])))
         event_message = event[0][event_index]
         print("\n<Event> " + event_message)
         if event_index == '1':
-            my_inventory.update({'(piece of gold)': int(my_inventory['(piece of gold)']) + 1 })
+            my_inventory.update({'(bag of gold)': int(my_inventory['(bag of gold)']) + 1 })
         elif event_index == '2':
-            myStatus['gold'] -= 150
+            my_status['gold'] -= random.randint(1, 100)
         elif event_index == '3':
-            myStatus['soldier'] += 100
+            my_status['soldier'] += random.randint(1,100)
         elif event_index == '4':
-            my_inventory.update({'(small package)': int(my_inventory['(small package)']) + 1 })
+            my_inventory.update({'(mysterious package)': int(my_inventory['(mysterious package)']) + 1 })
         elif event_index == '5':
             solve = input('\nWould you like to solve it? (yes/no): ')
             if solve.lower() == 'yes':
                 self.puzzle_solving()
             else:
                 print("\nYou found it boring and throw it away.")
-        if event_index == '6':
+        elif event_index == '6':
+            answer = input('\nWould you like to play with him? (yes/no): ').lower()
+            if answer == 'yes':
+                self.guessing_game()
+            else:
+                print('\nYou ignored him.')
+        elif event_index == '7':
+            answer = input('\nWould you like to find another way? (yes/no): ').lower
+            if answer == 'yes':
+                luck = random.randint(1,10)
+                if luck < 7:
+                    print("\n<System> You fall into the river. Next time don't take the risk! (lost 200 gold and 100 soldiers)")
+                    my_status['gold'] -= 200
+                    my_status['soldier'] -= 100
+                if luck > 7:
+                    print("\n<System> You safely passed through the river!")
+            else:
+                print("<System> You spent 30 minutes looking for another way and some of your soldiers run away due to the hot summer weather! (lost 100 soldiers)")
+                my_status['soldier'] -= 100
+        elif event_index == '8':
+            answer = input('\nWould you like to send your soldiers to fight the fire? (yes/no): ').lower()
+            if answer == 'yes':
+                luck = random.randint(1, 10)
+                if luck < 7:
+                    print("\n<System> After 2 hours of fighting, the village is safe!\nYou lost 200 soldiers and the villagers thank you a lot and gave you 500 gold! (- 200 soldiers & + 500 gold)")
+                    my_status['gold'] += 500
+                    my_status['soldier'] -= 200
+                else:
+                    print("\n<System> After 2 hours of fighting, the village is safe!\nThe villagers thank you a lot and gave you 500 gold! (+ 500 gold)")
+                    my_status['gold'] += 500
+            else:
+                print("\nYou run away with your soldiers and some of them are angry with you unresponsiveness! (-100 soldiers)")
+                my_status['soldier'] -= 100
+
+        elif event_index == '9':
             self.encounter_Oppo('lozar')
-        if event_index == '7':
+        elif event_index == '10':
             self.encounter_Oppo('nusta')
 
  #### Just for fun!       
@@ -223,7 +273,7 @@ class game:
         def correct():
             print(correct_message)
             gained_Resources = random.randint(50,200)
-            myStatus['gold'] += gained_Resources
+            my_status['gold'] += gained_Resources
             print("\n<System> You have gained " + str(gained_Resources) + " gold!")
         if puzzle_index == '1':
             if puzzle_answer == '35':
@@ -271,29 +321,12 @@ class game:
             else:
                 print(incorrect_message)
 
-#### Boss fighting :)
-    def encounter_Oppo(self, opponent):
-        decision = input("\nWould you like to attack the opponent's kingdom? (yes / no): ").lower()
-        if decision == "yes":
-            if opponent == "lozar":
-                self.opponent_index = 6
-                if myStatus["gold"] >= lozarStatus["gold"] and myStatus["soldier"] >= lozarStatus["soldier"]:
-                    self.attack(opponent)
-                else:
-                    print("\n<System> You do not have enough resources to attack.")
-            if opponent == "nusta":
-                self.opponent_index = 7
-                if myStatus["gold"] >= nustaStatus["gold"] and myStatus["soldier"] >= nustaStatus["soldier"]:
-                    pass
-                else:
-                    print("\n<System> You do not have the ability to attack.")
-        else:
-            print("\n<System> You run away before your opponent notice you.")
-        
-    def attack(self, opponent):
+#### Special Events
+    def guessing_game(self):
         lower_boundary = 1
         upper_boundary = 100
         bomb = random.randint(lower_boundary, upper_boundary)
+        print("\n<Mysterious Man> Young man, here is the game rule: I have randomly placed a BOMB in the number range 1 to 100. Who guess the bomb correctly will be the loser.")
         while True:
             my_guess = int(input('\nGuess a number within ' + str(lower_boundary) + ' - ' + str(upper_boundary) + ' : '))
             if my_guess <= lower_boundary or my_guess >= upper_boundary:
@@ -306,15 +339,13 @@ class game:
                 print("\n<System> The guess is smaller than bomb!")
                 lower_boundary = my_guess
             else:
-                print("\n<System> BOMB! You lost the battle!\nYou lost 1000 golds and 2000 soldiers!")
-                myStatus['gold'] -= 1000
-                myStatus['soldier'] -= 2000
+                print("\n<System> BOMB! You lost the game")
                 break
 
             time.sleep(2)
 
-            oppo_guess = random.randint(lower_boundary, upper_boundary)
-            print('\n<' + opponent + '> Guess: ' + str(oppo_guess))
+            oppo_guess = random.randint(lower_boundary+1, upper_boundary-1)
+            print('\n<Mysterious Man> Guess: ' + str(oppo_guess))
             if oppo_guess > bomb:
                 print("\n<System> The guess is larger than bomb!")
                 upper_boundary = oppo_guess
@@ -322,12 +353,88 @@ class game:
                 print("\n<System> The guess is smaller than bomb!")
                 lower_boundary = oppo_guess
             else:
-                print("\n<System> BOMB!\n\nCongratulations you have eliminate " + opponent + '!\nYou gained 1000 golds and 2000 soldiers from this victory')
-                myStatus['gold'] += 1000
-                myStatus['soldier'] += 2000
-                myStatus['victory'] += 1
+                print("\n<System> BOMB!\n\nCongratulations YOU WON!\nYou gained 1000 golds and 2000 soldiers from this victory")
+                my_status['gold'] += 1000
+                my_status['soldier'] += 2000
+                if my_inventory['(potion)'] == 0:
+                    my_inventory.update({'(potion)' : 1})
                 break
-        if myStatus['victory'] == 2:
+
+#### Boss fighting :)
+    def encounter_Oppo(self, opponent):
+        lozar_status['HP'] = 100
+        lozar_status['defend'] = 1 
+        nusta_status['HP'] = 100
+        nusta_status['defend'] = 1
+        my_status['HP'] = 100
+        my_status['defend'] = 1
+        decision = input("\nWould you like to attack the opponent's kingdom? The cost will be 1000 gold and 1000 soldiers (yes / no): ").lower()
+        if decision == "yes":
+            my_status['gold'] -= 1000
+            my_status['soldier'] -= 100=
+            if opponent == "lozar":
+                if my_status["gold"] >= lozar_status["gold"] and my_status["soldier"] >= lozar_status["soldier"]:
+                    self.fight('lozar')
+                else:
+                    print("\n<System> You do not have enough resources to attack.")
+            if opponent == "nusta":
+                if my_status["gold"] >= nusta_status["gold"] and my_status["soldier"] >= nusta_status["soldier"]:
+                    self.fight('nusta')
+                else:
+                    print("\n<System> You do not have the ability to attack.")
+        else:
+            print("\n<System> You run away before your opponent notice you.")
+
+#### Battle System       
+    def fight(self, opponent):
+        opponent_name = '\n<' + opponent + '> '
+        print(opponent_name + "Nice to see you here, my good 'friend'. So, let's fight!")
+        action = ['attack', 'defend']
+        while True:
+            if my_status['HP'] > 0:
+                my_action = input("\n<System> You turn. What action would you take? (attack/defend): ").lower()
+                if my_action == action[0]:
+                    if opponent == 'lozar':
+                        damage = int(random.randint(1, 20) / lozar_status['defend'])
+                        lozar_status['HP'] -= damage
+                        print("\n<System> " + opponent + ' lost ' + str(damage) + ' HP; he has ' + str(lozar_status['HP']) + ' left')
+                    elif opponent == 'nusta':
+                        damage = int(random.randint(1, 20) / nusta_status['defend'])
+                        print("\n<System> " + opponent + ' lost ' + str(damage) + ' HP; he has ' + str(nusta_status['HP']) + ' left')
+                        nusta_status['HP'] -= damage
+                elif my_action == action[1]:
+                    my_status['defend'] += 1
+                else:
+                    print('\n<System> Unknown action')
+                    continue         
+            else:
+                print('\n<System> You lost the battle! (- 2000 gold, - 2000 soldiers')
+                my_status['gold'] -= 2000
+                my_status['soldier'] -= 2000
+                break
+
+            if lozar_status['HP'] <= 0 or nusta_status['HP'] <= 0:
+                print('\n<System> You won the battle! (+ 2000 gold, + 2000 soldiers)')
+                my_status['gold'] += 2000
+                my_status['soldier'] += 2000
+                my_status['victory'] += 1
+                break
+
+            time.sleep(2)
+
+            opponent_action = action[random.randint(0,1)]
+            print(opponent_name + opponent_action)
+            if opponent_action == action[0]:
+                damage = int(random.randint(1,40) / my_status['defend'])
+                my_status['HP'] -= damage
+                print("\n<System> You lost " + str(damage) + " HP; you have " + str(my_status['HP']) + ' left')
+            elif opponent_action == action[1]:
+                if opponent == 'lozar':
+                    lozar_status['defend'] += 1
+                elif opponent == 'nusta':
+                    nusta_status['defend'] +=1
+            
+        if my_status['victory'] == 2:
             global victory_message
             print(victory_message)
             time.sleep(10)
@@ -336,20 +443,28 @@ class game:
 #### Actions
     def action_Use(self, item):
         if item in self.inventory_list:
-            gained_Resources = random.randint(50,200) * int(my_inventory[item])
-            myStatus['gold'] += gained_Resources
-            print("\n<System> Used " + item + " - You have gained " + str(gained_Resources) + " gold!")
-            my_inventory.update({item : 0})
+            if item == '(potion)':
+                if my_status['HP'] <= 30:
+                    my_status['HP'] += 50
+                    print("\n<System> Used potion and recovered 50% HP! Now your HP = " + str(my_status['HP']))
+                    my_inventory.update({item : 0})
+                else:
+                    print("\n<System> Your HP is higher than 30%")
+            else:
+                gained_Resources = random.randint(50,200) * int(my_inventory[item])
+                my_status['gold'] += gained_Resources
+                print("\n<System> Used " + item + " - You have gained " + str(gained_Resources) + " gold!")
+                my_inventory.update({item : 0})
         else:
             print('\n<System> Error: Invalid item')
 
     def action_Rest(self):
-        myStatus['gold'] -= random.randint(1,50)
-        myStatus['soldier'] += random.randint(1, 100)
-        myStatus['turn'] += 1
+        my_status['gold'] -= random.randint(1,50)
+        my_status['soldier'] += random.randint(1, 100)
+        my_status['turn'] += 1
     
     def action_Move(self):
-        myStatus['turn'] += 1
+        my_status['turn'] += 1
         self.event()
         
 # Execute the game
